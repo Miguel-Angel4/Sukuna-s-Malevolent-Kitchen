c01c4eb475330e59dbf77b38793faa1c41ff0e76
Alumno <alumno@example.com>
2026-04-17 13:09:15 +0200

    Estandarizar estructura Next.js: mover archivos a public y usar next.config.mjs para rewrites

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/form-submit',
+        destination: 'https://formsubmit.co/sukunaamalevolentkitchen@gmail.com',
+      },
+      {
+        source: '/',
+        destination: '/home.html',
+      },
+    ]
+  },
+}
 
 export default nextConfig
